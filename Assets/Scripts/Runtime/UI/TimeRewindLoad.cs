bf9d7ba24a7da1dcdf40f6fada762de4ab336c17
AlexBlackfrost <97742960+AlexBlackfrost@users.noreply.github.com>
2023-10-20 20:51:35 +0200

    Time rewind load circle UI animated, both fill and glow

--- Assets/Scripts/Runtime/UI/TimeRewindLoad.cs
+++ Assets/Scripts/Runtime/UI/TimeRewindLoad.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +6,82 @@ using UnityEngine.UI;
 
 public class TimeRewindLoad : MonoBehaviour{
     [SerializeField] private PlayerTimeRewinder playerTimeRewinder;
+    [SerializeField] private float glowAnimationDuration = 0.75f;
+    [SerializeField] private AnimationCurve glowAnimationCurve;
+    [SerializeField] private float defaultColorIntensity = 1.0f;
+    [SerializeField] private float maxColorIntensity = 1.2f;
+
     private Image image;
+    private Material material;
+    private Coroutine animateGlowCoroutine;
+    private bool isGlowing = false;
 
     private void Awake(){
         image = GetComponent<Image>();
+        material = image.material;
     }
 
     private void Update(){
-        image.fillAmount = playerTimeRewinder.GetRecordedDataRatio01();
+        if (!TimeRewindManager.Instance.IsRewinding) {
+            if (!isGlowing && image.fillAmount != 1) {
+                if (animateGlowCoroutine != null) {
+                    StopCoroutine(animateGlowCoroutine);
+                }
+                animateGlowCoroutine = StartCoroutine(AnimateGlow(maxColorIntensity));
+                isGlowing = true;
 
+            } else if (isGlowing && image.fillAmount == 1) {
+                if (animateGlowCoroutine != null) {
+                    StopCoroutine(animateGlowCoroutine);
+                }
+                animateGlowCoroutine = StartCoroutine(AnimateGlow(defaultColorIntensity));
+                isGlowing = false;
+            }
+        } 
+
+        image.fillAmount = playerTimeRewinder.GetRecordedDataRatio01();
+    }
+
+    private IEnumerator AnimateGlow(float targetColorIntensity) {
+        /* Tip for next time: use a target color and define it inspector instead of just the intensity, dealing  with color changes is a pain.
+         * Useful links:
+         * https://forum.unity.com/threads/how-to-change-hdr-colors-intensity-via-shader.531861/
+         * https://forum.unity.com/threads/understanding-srgb-and-gamma-corrected-values-in-the-render-pipeline.783224/
+         * https://discussions.unity.com/t/how-to-get-set-hdr-color-intensity/226028/5
+         */
+
+        Color32 originalColor;
+        float previousColorIntensity;
+        MathUtils.DecomposeHdrColor(material.color, out originalColor, out previousColorIntensity);
+        Color originalColorScaled = new Color(originalColor.r / 255.0f *2.0f, originalColor.g / 255.0f *2.0f, originalColor.b / 255.0f * 2.0f, originalColor.a);
+        Color originalColorGammaSpace = new Color(Mathf.LinearToGammaSpace(originalColorScaled.r),
+                                                  Mathf.LinearToGammaSpace(originalColorScaled.g),
+                                                  Mathf.LinearToGammaSpace(originalColorScaled.b),
+                                                  material.color.a);
+
+        float changeGlowSpeed = Mathf.Abs(targetColorIntensity - previousColorIntensity) / glowAnimationDuration;
+        float glowAnimationElapsedTime = 0;
+
+        while (glowAnimationElapsedTime < glowAnimationDuration) {
+            float lerpAlpha = glowAnimationCurve.Evaluate(glowAnimationElapsedTime * changeGlowSpeed);
+            float currentColorIntensity = Mathf.Lerp(previousColorIntensity, targetColorIntensity, lerpAlpha);
+
+            float scaledIntensity = Mathf.LinearToGammaSpace(Mathf.Pow(2.0f, currentColorIntensity-1));
+            material.color = new Color(Mathf.GammaToLinearSpace(originalColorGammaSpace.r * scaledIntensity),
+                                       Mathf.GammaToLinearSpace(originalColorGammaSpace.g * scaledIntensity),
+                                       Mathf.GammaToLinearSpace(originalColorGammaSpace.b * scaledIntensity),
+                                       material.color.a);
+
+            glowAnimationElapsedTime += Time.deltaTime;
+            // Be careful with linear and gamma color spaces https://forum.unity.com/threads/how-to-change-hdr-colors-intensity-via-shader.531861/#post-3501895
+            yield return null;
+        }
+
+        float targetIntensity = Mathf.LinearToGammaSpace(Mathf.Pow(2.0f, targetColorIntensity - 1));
+        material.color = new Color(Mathf.GammaToLinearSpace(originalColorGammaSpace.r * targetIntensity),
+                                   Mathf.GammaToLinearSpace(originalColorGammaSpace.g * targetIntensity),
+                                   Mathf.GammaToLinearSpace(originalColorGammaSpace.b * targetIntensity),
+                                   material.color.a);
+        animateGlowCoroutine = null;
     }
 }
--- Assets/Scripts/Runtime/Utils/MathUtils.cs
+++ Assets/Scripts/Runtime/Utils/MathUtils.cs
@@ -1,6 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+
+
 
 public static class MathUtils {
     public static float MapRangeUnclamped(float value, float inMin, float inMax, float outMin, float outMax) {
@@ -20,5 +24,43 @@ public static class MathUtils {
         position1.y = 0;
         position2.y = 0;
         return Vector3.Distance(position1, position2);
+    }
+
+    public static float GetHdrColorIntensity(Color linearColorHdr) {
+        const byte MAX_BYTE_FOR_OVEREXPOSED_COLOR = 191;
+        float maxColorComponent = linearColorHdr.maxColorComponent;
+        float intensity = 0;
+        // replicate Photoshops's decomposition behaviour
+        if (! (maxColorComponent == 0f || maxColorComponent <= 1f && maxColorComponent >= 1 / 255f) ) {
+            // calibrate exposure to the max float color component
+            float scaleFactor = MAX_BYTE_FOR_OVEREXPOSED_COLOR / maxColorComponent;
+            intensity = Mathf.Log(255f / scaleFactor) / Mathf.Log(2f);
+        }
+        return intensity;
+    }
+
+    public static void DecomposeHdrColor(Color linearColorHdr, out Color32 baseLinearColor, out float exposure) {
+        const byte MAX_BYTE_FOR_OVEREXPOSED_COLOR = 191;
+        const byte MAX_BYTE_LINEAR_COLOR = 255;
+        baseLinearColor = linearColorHdr;
+        float maxColorComponent = linearColorHdr.maxColorComponent;
+        // replicate Photoshops's decomposition behaviour
+        if (maxColorComponent == 0f || maxColorComponent <= 1f && maxColorComponent >= 1 / 255f) {
+            exposure = 0f;
+
+            baseLinearColor.r = (byte)Mathf.RoundToInt(linearColorHdr.r * 255f);
+            baseLinearColor.g = (byte)Mathf.RoundToInt(linearColorHdr.g * 255f);
+            baseLinearColor.b = (byte)Mathf.RoundToInt(linearColorHdr.b * 255f);
+        } else {
+            // calibrate exposure to the max float color component
+            float scaleFactor = MAX_BYTE_FOR_OVEREXPOSED_COLOR / maxColorComponent;
+            exposure = Mathf.Log(255f / scaleFactor) / Mathf.Log(2f);
+
+            scaleFactor = MAX_BYTE_LINEAR_COLOR / maxColorComponent;
+            // maintain maximal integrity of byte values to prevent off-by-one errors when scaling up a color one component at a time
+            baseLinearColor.r = Math.Min(MAX_BYTE_FOR_OVEREXPOSED_COLOR, (byte)Mathf.CeilToInt(scaleFactor * linearColorHdr.r));
+            baseLinearColor.g = Math.Min(MAX_BYTE_FOR_OVEREXPOSED_COLOR, (byte)Mathf.CeilToInt(scaleFactor * linearColorHdr.g));
+            baseLinearColor.b = Math.Min(MAX_BYTE_FOR_OVEREXPOSED_COLOR, (byte)Mathf.CeilToInt(scaleFactor * linearColorHdr.b));
+        }
     }
 }
