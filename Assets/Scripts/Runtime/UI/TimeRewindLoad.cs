d55da8eef3f34dd4ae1d5589f72e571be31d680b
AlexBlackfrost <97742960+AlexBlackfrost@users.noreply.github.com>
2023-10-20 21:10:05 +0200

    Changed glow animation implementation. Now it is based on color lerping rather than hdr intensity lerping

--- Assets/Scripts/Runtime/UI/TimeRewindLoad.cs
+++ Assets/Scripts/Runtime/UI/TimeRewindLoad.cs
@@ -10,6 +10,8 @@ public class TimeRewindLoad : MonoBehaviour{
     [SerializeField] private AnimationCurve glowAnimationCurve;
     [SerializeField] private float defaultColorIntensity = 1.0f;
     [SerializeField] private float maxColorIntensity = 1.2f;
+    [SerializeField][ColorUsageAttribute(true, true)] private Color defaultColor;
+    [SerializeField][ColorUsageAttribute(true, true)] private Color glowColor;
 
     private Image image;
     private Material material;
@@ -27,14 +29,14 @@ public class TimeRewindLoad : MonoBehaviour{
                 if (animateGlowCoroutine != null) {
                     StopCoroutine(animateGlowCoroutine);
                 }
-                animateGlowCoroutine = StartCoroutine(AnimateGlow(maxColorIntensity));
+                animateGlowCoroutine = StartCoroutine(AnimateGlow(glowColor));
                 isGlowing = true;
 
             } else if (isGlowing && image.fillAmount == 1) {
                 if (animateGlowCoroutine != null) {
                     StopCoroutine(animateGlowCoroutine);
                 }
-                animateGlowCoroutine = StartCoroutine(AnimateGlow(defaultColorIntensity));
+                animateGlowCoroutine = StartCoroutine(AnimateGlow(defaultColor));
                 isGlowing = false;
             }
         } 
@@ -42,46 +44,78 @@ public class TimeRewindLoad : MonoBehaviour{
         image.fillAmount = playerTimeRewinder.GetRecordedDataRatio01();
     }
 
-    private IEnumerator AnimateGlow(float targetColorIntensity) {
+    //private IEnumerator AnimateGlow(float targetColorIntensity) {
+    /* Tip for next time: use a target color and define it inspector instead of just the intensity, dealing  with color changes is a pain.
+     * Useful links:
+     * https://forum.unity.com/threads/how-to-change-hdr-colors-intensity-via-shader.531861/
+     * https://forum.unity.com/threads/understanding-srgb-and-gamma-corrected-values-in-the-render-pipeline.783224/
+     * https://discussions.unity.com/t/how-to-get-set-hdr-color-intensity/226028/5
+     */
+
+    /*Color32 originalColor;
+    float previousColorIntensity;
+    MathUtils.DecomposeHdrColor(material.color, out originalColor, out previousColorIntensity);
+    Color originalColorScaled = new Color(originalColor.r / 255.0f *2.0f, originalColor.g / 255.0f *2.0f, originalColor.b / 255.0f * 2.0f, originalColor.a);
+    Color originalColorGammaSpace = new Color(Mathf.LinearToGammaSpace(originalColorScaled.r),
+                                              Mathf.LinearToGammaSpace(originalColorScaled.g),
+                                              Mathf.LinearToGammaSpace(originalColorScaled.b),
+                                              material.color.a);
+
+    float changeGlowSpeed = Mathf.Abs(targetColorIntensity - previousColorIntensity) / glowAnimationDuration;
+    float glowAnimationElapsedTime = 0;
+
+    while (glowAnimationElapsedTime < glowAnimationDuration) {
+        float lerpAlpha = glowAnimationCurve.Evaluate(glowAnimationElapsedTime * changeGlowSpeed);
+        float currentColorIntensity = Mathf.Lerp(previousColorIntensity, targetColorIntensity, lerpAlpha);
+        if (currentColorIntensity > 1) {
+            currentColorIntensity--;
+        }
+        float scaledIntensity = Mathf.LinearToGammaSpace(Mathf.Pow(2.0f, currentColorIntensity));
+        material.color = new Color(Mathf.GammaToLinearSpace(originalColorGammaSpace.r * scaledIntensity),
+                                   Mathf.GammaToLinearSpace(originalColorGammaSpace.g * scaledIntensity),
+                                   Mathf.GammaToLinearSpace(originalColorGammaSpace.b * scaledIntensity),
+                                   material.color.a);
+
+        glowAnimationElapsedTime += Time.deltaTime;
+        // Be careful with linear and gamma color spaces https://forum.unity.com/threads/how-to-change-hdr-colors-intensity-via-shader.531861/#post-3501895
+        yield return null;
+    }
+
+    float targetIntensity = Mathf.LinearToGammaSpace(Mathf.Pow(2.0f, targetColorIntensity ));
+    if(targetIntensity > 1) {
+        targetIntensity--;
+    }
+    material.color = new Color(Mathf.GammaToLinearSpace(originalColorGammaSpace.r * targetIntensity),
+                               Mathf.GammaToLinearSpace(originalColorGammaSpace.g * targetIntensity),
+                               Mathf.GammaToLinearSpace(originalColorGammaSpace.b * targetIntensity),
+                               material.color.a);
+    animateGlowCoroutine = null;
+}*/
+
+    private IEnumerator AnimateGlow(Color targetColor) {
         /* Tip for next time: use a target color and define it inspector instead of just the intensity, dealing  with color changes is a pain.
          * Useful links:
          * https://forum.unity.com/threads/how-to-change-hdr-colors-intensity-via-shader.531861/
          * https://forum.unity.com/threads/understanding-srgb-and-gamma-corrected-values-in-the-render-pipeline.783224/
          * https://discussions.unity.com/t/how-to-get-set-hdr-color-intensity/226028/5
          */
-
-        Color32 originalColor;
-        float previousColorIntensity;
-        MathUtils.DecomposeHdrColor(material.color, out originalColor, out previousColorIntensity);
-        Color originalColorScaled = new Color(originalColor.r / 255.0f *2.0f, originalColor.g / 255.0f *2.0f, originalColor.b / 255.0f * 2.0f, originalColor.a);
-        Color originalColorGammaSpace = new Color(Mathf.LinearToGammaSpace(originalColorScaled.r),
-                                                  Mathf.LinearToGammaSpace(originalColorScaled.g),
-                                                  Mathf.LinearToGammaSpace(originalColorScaled.b),
-                                                  material.color.a);
-
-        float changeGlowSpeed = Mathf.Abs(targetColorIntensity - previousColorIntensity) / glowAnimationDuration;
+        Color previousColor = material.color;
+        float changeGlowSpeed = Mathf.Abs(targetColor.maxColorComponent - previousColor.maxColorComponent) / glowAnimationDuration;
         float glowAnimationElapsedTime = 0;
 
         while (glowAnimationElapsedTime < glowAnimationDuration) {
             float lerpAlpha = glowAnimationCurve.Evaluate(glowAnimationElapsedTime * changeGlowSpeed);
-            float currentColorIntensity = Mathf.Lerp(previousColorIntensity, targetColorIntensity, lerpAlpha);
+            Color currentColor = Color.Lerp(previousColor, targetColor, lerpAlpha);
 
-            float scaledIntensity = Mathf.LinearToGammaSpace(Mathf.Pow(2.0f, currentColorIntensity-1));
-            material.color = new Color(Mathf.GammaToLinearSpace(originalColorGammaSpace.r * scaledIntensity),
-                                       Mathf.GammaToLinearSpace(originalColorGammaSpace.g * scaledIntensity),
-                                       Mathf.GammaToLinearSpace(originalColorGammaSpace.b * scaledIntensity),
-                                       material.color.a);
+            material.color = currentColor;
 
             glowAnimationElapsedTime += Time.deltaTime;
             // Be careful with linear and gamma color spaces https://forum.unity.com/threads/how-to-change-hdr-colors-intensity-via-shader.531861/#post-3501895
             yield return null;
         }
 
-        float targetIntensity = Mathf.LinearToGammaSpace(Mathf.Pow(2.0f, targetColorIntensity - 1));
-        material.color = new Color(Mathf.GammaToLinearSpace(originalColorGammaSpace.r * targetIntensity),
-                                   Mathf.GammaToLinearSpace(originalColorGammaSpace.g * targetIntensity),
-                                   Mathf.GammaToLinearSpace(originalColorGammaSpace.b * targetIntensity),
-                                   material.color.a);
+
+        material.color = targetColor;
         animateGlowCoroutine = null;
     }
 }
